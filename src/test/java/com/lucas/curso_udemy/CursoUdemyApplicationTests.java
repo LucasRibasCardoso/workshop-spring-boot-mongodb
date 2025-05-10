0335e1dbc6398a490053bf2a245ec1299ecc9e40
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-05-09 22:47:28 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/curso_udemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.curso_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
