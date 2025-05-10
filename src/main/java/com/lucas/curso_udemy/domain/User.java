d0a81e1b43d8a4c109ea59ae7c009907a1fab4dc
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-05-10 20:31:00 -0300

    Adicionado algumas anotacoes na classe e no id

--- src/main/java/com/lucas/curso_udemy/domain/User.java
+++ src/main/java/com/lucas/curso_udemy/domain/User.java
@@ -1,13 +1,18 @@
 package com.lucas.curso_udemy.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document // Utilizado para informar que é um entidade do MongoDB
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
