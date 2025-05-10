c83bbc54ce2a228c3568053681097f76d9665d96
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-05-10 20:31:34 -0300

    Criado o UserService

---
+++ src/main/java/com/lucas/curso_udemy/service/UserService.java
@@ -0,0 +1,20 @@
+package com.lucas.curso_udemy.service;
+
+import com.lucas.curso_udemy.domain.User;
+import com.lucas.curso_udemy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
