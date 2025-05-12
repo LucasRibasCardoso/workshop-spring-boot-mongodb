d8ffcad4c01c3e3cab45fd4308b06cfc1fd323a9
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-05-12 18:28:03 -0300

    Alterado a busca de posts pelo  titulo para uma query manual

--- src/main/java/com/lucas/curso_udemy/repository/PostRepository.java
+++ src/main/java/com/lucas/curso_udemy/repository/PostRepository.java
@@ -2,11 +2,20 @@ package com.lucas.curso_udemy.repository;
 
 import com.lucas.curso_udemy.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
-    List<Post> findByTitleContainingIgnoreCase(String text);
+
+
+    /**
+     * Poderia ser substituída pelo queryMethod findByTitleContainingIgnoreCase.
+     * @param text
+     * @return uma lista de posts que no campo title, contenham o valor text, ignorando maiusculas e minusculas.
+     */
+    @Query("{ 'title':  { $regex:  ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
